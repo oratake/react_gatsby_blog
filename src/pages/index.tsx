cb7b6ef42899b24a05977a7103aecdda4bddeddf
oratake <smith.shimomura@gmail.com>
2021-06-05 16:48:40 +0900

    feat: add new index.js

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import type { FC } from 'react';
+
+const Page: FC = () => (
+  <div>
+    <h1>Home</h1>
+    <p>Hello, Gatsby.js!</p>
+  </div>
+);
+
+export default Page;
