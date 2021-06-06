bfb7e973097304db39e6746ac3c5942348b1ca2c
oratake <smith.shimomura@gmail.com>
2021-06-07 08:37:28 +0900

    feat: add link index to about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import type { FC } from 'react';
+import { Link } from 'gatsby';
 
 const Page: FC = () => (
   <div>
     <h1>About</h1>
     <p>about page.</p>
+    <Link to="/">Home</Link>
   </div>
 );
 
