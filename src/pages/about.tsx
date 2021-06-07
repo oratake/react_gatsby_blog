4a6cb3e3b65c6f61192c8e3a7d0c148db335ce81
oratake <smith.shimomura@gmail.com>
2021-06-07 20:09:02 +0900

    feat: insert header and footer on each pages

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import type { FC } from 'react';
 import { Link } from 'gatsby';
+import { Header } from '../components/header';
+import { Footer } from '../components/footer';
 
 const Page: FC = () => (
   <div>
+    <Header />
     <h1>About</h1>
     <p>about page.</p>
     <Link to="/">Home</Link>
+    <Footer />
   </div>
 );
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import type { FC } from 'react';
 import { Link } from 'gatsby';
+import { Header } from '../components/header';
+import { Footer } from '../components/footer';
 
 const Page: FC = () => (
   <div>
+    <Header />
     <h1>Home</h1>
     <p>Hello, Gatsby.js!</p>
     <Link to="/about">About this blog</Link>
+    <Footer />
   </div>
 );
 
