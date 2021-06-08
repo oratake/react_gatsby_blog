150962686769e81307fd9291bb99cf42ceedde13
oratake <smith.shimomura@gmail.com>
2021-06-09 08:23:27 +0900

    feat: add metadata

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,6 +1,12 @@
 import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
+  siteMetadata: {
+    title: 'Blog title',
+    author: 'Shoichi Yokoi',
+    description: 'Example blog using GatsbyJS',
+    email: 'example@example.com'
+  },
   plugins: ['gatsby-plugin-sass']
 };
 export default config;
