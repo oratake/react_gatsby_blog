69663930bead873bb5406beb1a8c4a9e84ea0007
oratake <smith.shimomura@gmail.com>
2021-06-12 15:01:20 +0900

    feat: add types on page component

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import type { FC } from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql } from 'gatsby';
 import { Layout } from '../components/layout';
 
-const Page: FC = () => (
+interface PageProps {
+  data: GatsbyTypes.AboutPageQuery;
+}
+
+const Page: FC<PageProps> = ({ data }) => (
   <Layout>
-    <h1>About</h1>
+    <h1>About {data.site?.siteMetadata?.title ?? '(無題)'}</h1>
     <p>about page.</p>
     <Link to="/">Home</Link>
   </Layout>
 );
 
 export default Page;
+
+export const query = graphql`
+  query AboutPage {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
